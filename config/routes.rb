6d7f803867249fcae441ad59643c0e23965e0e9d
Elana Ryznar <eryzna@gmail.com>
2020-02-29 04:36:02 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+  get 'authors/:id/posts'
+  get 'authors/:id/posts/:post_id'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
